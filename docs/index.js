9cfca995195ebe3df739fe09abd8d1bc0a29c730
allyson-y <ayao20@holynames-sea.org>
2022-02-16 10:31:42 -0800

    edited initialData var

---
+++ docs/index.js
@@ -0,0 +1,14 @@
+const initialData = [
+    {
+        id: "1276",
+        isChecked: false,
+        textInput: "Buy Book",
+        isDone: false
+    },
+    {
+        id: "787",
+        isChecked: true,
+        textInput: "Take trash out",
+        isDone: true
+    }
+];
