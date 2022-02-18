04a5d8ca7ce4441d2f312b5e399e441d1ecd7b8e
Yurie Muramatsu <yuriemuramatsu@Yuries-MacBook-Air.local>
2022-02-17 21:51:11 -0800

    struggling

--- react-app/checkbox.js
+++ react-app/checkbox.js
@@ -3,11 +3,29 @@ import React from "react";
 // import App from "./src/App";
 import {useState} from "react";
 import './index.js';
+import 'screen1.html';
 
+// const element = React.createElement;
+const checkboxButton = document.getElementById("completion");
+const taskInput = document.getElementById("task");
+const deleteButton = document.getElementById("delete");
 
-const element = React.createElement;
-const completion = document.getElementById("completion");
+console.log("hi");
 
+deleteButton.addEventListener("click", () =>{
+    console.log("test")
+    alert("hello!");
+})
+
+checkboxButton.addEventListener("click", () => {
+    // count+=1;
+    console.log("hello")
+}, false)
+
+taskInput.addEventListener("click", () => {
+    console.log("test")
+    alert("hello!");
+}, false)
 
 // class checkbox extends React.Component {
 //     constructor(props) {
@@ -37,50 +55,63 @@ const completion = document.getElementById("completion");
 
 const data = "initialData";
 
-function checkbox() {
+// function checkbox() {
+//
+//     // const data = initialData;
+//
+//     function People_List(props) {
+//         let [selectedId, setSelectedId] = useState(null);
+//         let numberSelected = selectedId ? 1 : 0;
+//
+//         console.log(selectedId);
+//
+//         return (
+//             <div>
+//                 <h1>People {numberSelected}/ {props.people.length} selected</h1>,
+//                 <table>
+//                     {props.people.map((e) =>
+//                         <Person_Item onSelectPerson={setSelectedId} id={e.id} key={e.id} name={e.name} email={e.email}
+//                                      phone={e.phone}/>)}
+//                 </table>,
+//             </div>);
+//     }
+//
+//     function Person_Item(props) {
+//
+//         function handleClick(e) {
+//             console.log("\nhandleClick")
+//             let selectedId = e.target.id;
+//             console.log(selectedId);
+//             return e.target.id;
+//         }
+//
+//         return <div onClick={() => props.onSelectPerson(props.id)}>
+//             <tr id={props.id}>
+//                 <td>{props.name}</td>
+//                 <td>{props.email}</td>
+//                 <td>{props.phone}</td>
+//             </tr>
+//         </div>
+//     }
+//
+//     taskInput.addEventListener("click", () => {
+//         console.log("test")
+//         alert("hello!");
+//     })
+//
+//     let count = 0;
+//     checkboxButton.addEventListener("click", () => {
+//         count+=1;
+//         console.log("hello")
+//     })
+//
+//     console.log(count);
+//
+//     return (
+//         <div>
+//             <People_List people={data}/>
+//         </div>
+//     );
+// }
 
-    // const data = initialData;
-
-    function People_List(props) {
-        let [selectedId, setSelectedId] = useState(null);
-        let numberSelected = selectedId ? 1 : 0;
-
-        console.log(selectedId);
-
-        return <div>
-            <h1>People {numberSelected}/ {props.people.length} selected</h1>
-            <table>
-                {props.people.map((e) =>
-                    <Person_Item onSelectPerson={setSelectedId} id={e.id} key={e.id} name={e.name} email={e.email}
-                                 phone={e.phone}/>)}
-            </table>
-        </div>
-    }
-
-    function Person_Item(props) {
-
-        function handleClick(e) {
-            console.log("\nhandleClick")
-            let selectedId = e.target.id;
-            console.log(selectedId);
-            return e.target.id;
-        }
-
-        return <div onClick={() => props.onSelectPerson(props.id)}>
-            <tr id={props.id}>
-                <td>{props.name}</td>
-                <td>{props.email}</td>
-                <td>{props.phone}</td>
-            </tr>
-        </div>
-    }
-
-    return (
-        <div>
-            <People_List people={data}/>
-        </div>
-    );
-}
-
-
-export default checkbox();
+// export default checkbox();
