ad3c01a6f1b1a03a4b54f5b6310d7bce07d16b28
allyson-y <ayao20@holynames-sea.org>
2022-02-28 15:55:13 -0800

    implemented task

---
+++ react-app/src/task.js
@@ -0,0 +1,39 @@
+import Checkbox from "./checkbox";
+import TextInput from "./textInput";
+import React, {useState} from "react";
+import './task.css';
+
+function Task(props) {
+    // checked holds all checkboxes that are checked
+    let [checked, setChecked] = useState(false);
+
+    let thisId = "task1";
+
+    console.log(props.isChecked)
+
+    if (props.isChecked === true){
+        thisId = "task2"
+    }
+
+    function changeColor() {
+        setChecked(true);
+        console.log("changeColor is called")
+    }
+
+    return <body className="task">
+    <input
+        className="checkbox"
+        type={"checkbox"}
+        onClick={props.colorChanger}
+        colorChanger = {changeColor}
+    />
+    <input
+        type="text"
+        name="task1"
+        id = {thisId}
+        isChecked = {checked}
+    />
+    </body>
+}
+
+export default Task;
